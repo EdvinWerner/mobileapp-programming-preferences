05f7e31a8e21f179dbc7b540cd41e62eeaeab77a
Edde2508 <edvin.werner@live.se>
2024-05-03 12:53:59 +0200

    Made it possible for the user to add and change his/her preferance

--- app/src/main/java/com/example/project/Activty2.java
+++ app/src/main/java/com/example/project/Activty2.java
@@ -1,15 +1,45 @@
 package com.example.project;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Activty2 extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activty2);
+
+        myPreferenceRef = getSharedPreferences("key", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        EditText prefTextRef = findViewById(R.id.my_edittext);
+        prefTextRef.setText(myPreferenceRef.getString("key", "No preference found."));
+
+        Button Submit = findViewById(R.id.my_button2);
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                store();
+                Intent intent = new Intent(Activty2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    void store() {
+        EditText sharedPrefData = findViewById(R.id.my_edittext);
+        myPreferenceEditor.putString("key", sharedPrefData.getText().toString());
+        myPreferenceEditor.apply();
     }
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.project;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +22,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        myPreferenceRef = getSharedPreferences("key", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        TextView prefTextRef= findViewById(R.id.my_textview);
+        prefTextRef.setText(myPreferenceRef.getString("key", "No preference found."));
 
         Button ToSecondActivity = findViewById(R.id.my_button1);
         ToSecondActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent;
-                intent = new Intent(MainActivity.this, Activty2.class);
+                Intent intent = new Intent(MainActivity.this, Activty2.class);
                 startActivity(intent);
             }
         });
-
-    }}
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView sharedPrefData = findViewById(R.id.my_textview);
+        sharedPrefData.setText(myPreferenceRef.getString("key", "Hello!"));
+    }
+}
